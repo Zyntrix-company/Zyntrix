e6c379afd61be071a023977957870daef65b5b8f
Vanshahuja1 <139003501+Vanshahuja1@users.noreply.github.com>
2025-11-02 12:43:22 +0530

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,22 +1,14 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  exclude: ['/404'],
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  transform: async (config, path) => {
-    return {
-      loc: path,
-      changefreq: 'weekly',
-      priority: path === '/' ? 1.0 : 0.7,
-      lastmod: new Date().toISOString(),
-      alternateRefs: [],
-    };
+  typescript: {
+    ignoreBuildErrors: true,
   },
-  // additionalPaths: async (config) => {
-  //   return [];
-  // },
-};
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
